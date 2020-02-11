560822450c565ad5789d61ba8198b9c77e9714ae
wangcanyi <wangcanyi@usoftchina.com>
2020-02-11 17:47:40 +0800

    dll 打包

--- package.sh
+++ package.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-projectDir=/mnt/c/Users/wangcanyi/Documents/git-csharp/usoft-sdk-b2b-csharp
-
+projectDir='/mnt/c/Users/wangcanyi/Documents/git-csharp/usoft-sdk-b2b-csharp'
 cd $projectDir/usoft-sdk-b2b-csharp/bin/Debug
-
-zip $projectDir/dll-release/usoft-sdk-b2b-csharp.zip ./*
-
+/usr/bin/zip $projectDir/dll-release/usoft-sdk-b2b-csharp.zip *.*
